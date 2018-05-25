82b8e71fab2e5952eb80f5a362d95482e20d02f2
Nundy <18710939328@163.com>
2018-05-25 19:59:54 +0800

    init

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -78,23 +78,6 @@ var webpackConfig = merge(baseWebpackConfig, {
   }
 })
 
-if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require('compression-webpack-plugin')
-
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
-      ),
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  )
-}
 
 if (config.build.bundleAnalyzerReport) {
   var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
